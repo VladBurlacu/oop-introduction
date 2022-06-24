53e05b1859150fa3fb159a21d8683ecb8681cdd0
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-06-24 12:07:36 +0200

    finished exercise 5

--- exercise_5_public.php
+++ exercise_5_public.php
@@ -2,6 +2,36 @@
 
 declare(strict_types=1);
 
+class Beverage
+{
+    private string $name;
+    private string $color;
+    private float $price;
+    private string $temperature;
+
+    public function __construct(string $name, string $color, float $price)
+    {
+        $this->name = $name;
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = "cold";
+    }
+
+    public function changePrice(float $newPrice){
+        if ($newPrice > 2){
+            $this->price = $newPrice;
+            return "Price of a $this->name is: $this->price";
+        }
+    }
+}
+
+$cola = new Beverage("cola", "black", 2);
+
+echo $cola->changePrice(3.5);
+
+
+
+
 /* EXERCISE 5
 
 Copy the class of exercise 1.
